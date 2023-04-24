8c1b7cd74036e7381e0a5d3b19dad16d300dc565
Danny Guinther <dguinther@neuralmagic.com>
2023-04-24 16:26:25 -0400

    Include id_token with password grant

--- aioauth/oidc/core/grant_type.py
+++ aioauth/oidc/core/grant_type.py
@@ -9,6 +9,7 @@ Different OAuth 2.0 grant types with OpenID Connect extensions.
 """
 from aioauth.grant_type import (
     AuthorizationCodeGrantType as OAuth2AuthorizationCodeGrantType,
+    PasswordGrantType as OAuth2PasswordGrantType,
 )
 from aioauth.models import Client
 from aioauth.oidc.core.responses import TokenResponse
@@ -78,6 +79,52 @@ class AuthorizationCodeGrantType(OAuth2AuthorizationCodeGrantType[TRequest, TSto
             request,
             client.client_id,
             request.post.code,
+        )
+
+        return TokenResponse(
+            access_token=token.access_token,
+            expires_in=token.expires_in,
+            id_token=id_token,
+            refresh_token=token.refresh_token,
+            refresh_token_expires_in=token.refresh_token_expires_in,
+            scope=token.scope,
+            token_type=token.token_type,
+        )
+
+
+class PasswordGrantType(OAuth2PasswordGrantType[TRequest, TStorage]):
+    """
+    The Password grant type is a way to exchange a user's credentials
+    for an access token. Because the client application has to collect
+    the user's password and send it to the authorization server, it is
+    not recommended that this grant be used at all anymore.
+    See `RFC 6749 section 1.3.3 <https://tools.ietf.org/html/rfc6749#section-1.3.3>`_.
+    The latest `OAuth 2.0 Security Best Current Practice <https://tools.ietf.org/html/draft-ietf-oauth-security-topics-13#section-3.4>`_
+    disallows the password grant entirely.
+    """
+
+    async def create_token_response(
+        self, request: TRequest, client: Client
+    ) -> TokenResponse:
+        """Creates token response to reply to client."""
+        if self.scope is None:
+            raise RuntimeError("validate_request() must be called first")
+
+        token = await self.storage.create_token(
+            request,
+            client.client_id,
+            self.scope,
+            generate_token(42),
+            generate_token(48),
+        )
+
+        id_token = await self.storage.get_id_token(
+            client_id=client.client_id,
+            nonce=None,
+            redirect_uri=request.query.redirect_uri,
+            request=request,
+            response_type="code",
+            scope=self.scope,
         )
 
         return TokenResponse(
